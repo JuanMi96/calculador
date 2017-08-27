f6fc86979c7595a1694725de3badeb7d0ec47c68
Daniel <Daniel@DESKTOP-79T5FQV>
2017-08-27 18:42:02 -0500

    2

---
+++ JavaApplication3/src/Calculador.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Daniel
+ */
+public class Calculador {
+    
+    float operador1;
+    float operador2;
+    float resultado;
+    
+    void sumar(){
+        
+        resultado=operador1+operador2;
+                
+    }
+
+    void restar() {
+        
+        resultado=operador1-operador2;
+        
+    }
+    
+    
+    
+} 
